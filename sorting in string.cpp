edf9c4edb774700bb59d78afebefdb8fb423f547
Justin-roy <69432186+Justin-roy@users.noreply.github.com>
2021-03-09 08:20:46 +0000

    Add files via upload

---
+++ sorting in string.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s = "skdhfliuafei";
+
+    sort(s.begin(),s.end());
+    cout<<s<<endl;
+return 0;
+}
